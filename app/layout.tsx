3a962a95472b690c689381e5092946dc9815aebc
sojinsamuel <sojinsamuel2001@gmail.com>
2023-11-09 16:39:24 +0530

    add crisp provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="mx-auto max-w-7xl pt-16  flex-grow">
-              {/* <CrispProvider /> */}
+              <CrispProvider />
               {children}
             </main>
             {/* <footer className="w-full flex items-center justify-center py-3">
